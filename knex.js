e1b7c1d91c58ef2fc8d4784de53aeb877ed71c52
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-29 14:35:36 +0900

    deploy

--- knex.js
+++ knex.js
@@ -1,13 +1,7 @@
-// import { config } from "./knexfile";
-// const config = require('./knexfile');
-// const environment = process.env.NODE_ENV;
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+require("dotenv").config();
 
-// const knex = require('knex')(config);
-
-// module.exports = knex;
-
-const knex = require('knex');
-const knexConfig = require('./knexfile');
 const environment = process.env.NODE_ENV;
 
 module.exports = knex(knexConfig[environment]);
--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,38 @@
 // Update with your config settings.
 // require("dotenv").config();
 require('dotenv').config({
-  path: './.env',
+	path: './.env',
 });
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      // host: process.env.DB_HOST || "127.0.0.1",
-      // port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    // ...
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-  },
+	development: {
+		client: 'pg',
+		connection: {
+			// host: process.env.DB_HOST || "127.0.0.1",
+			// port: process.env.DB_PORT || 5432,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+		},
+		migrations: {
+			directory: './db/migrations',
+		},
+		seeds: {
+			directory: './db/seeds',
+		},
+	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		// ...
+		migrations: {
+			directory: './db/migrations',
+		},
+		seeds: {
+			directory: './db/seeds',
+		},
+	},
 };
