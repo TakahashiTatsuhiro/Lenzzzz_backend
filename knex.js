6f7cc083b935a6a66254180348634bf44be9fac8
shogo <shogo_inoue@mail.toyota.co.jp>
2023-11-29 14:22:01 +0900

    third

--- knex.js
+++ knex.js
@@ -1,8 +1,13 @@
 // import { config } from "./knexfile";
-const config = require("./knexfile");
-const knex = require("knex")(config);
-// import { knex } from "knex";
-// require("dotenv").config();
-// const knexConfig = knex(config);
+// const config = require('./knexfile');
+// const environment = process.env.NODE_ENV;
 
-module.exports = knex;
+// const knex = require('knex')(config);
+
+// module.exports = knex;
+
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const environment = process.env.NODE_ENV;
+
+module.exports = knex(knexConfig[environment]);
--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,38 @@
 // Update with your config settings.
 // require("dotenv").config();
-require("dotenv").config({
-  path: "./.env",
+require('dotenv').config({
+  path: './.env',
 });
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
 module.exports = {
-  client: "pg",
-  connection: {
-    // host: process.env.DB_HOST || "127.0.0.1",
-    // port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  development: {
+    client: 'pg',
+    connection: {
+      // host: process.env.DB_HOST || "127.0.0.1",
+      // port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
   },
-  migrations: {
-    directory: "./db/migrations",
-  },
-  seeds: {
-    directory: "./db/seeds",
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // ...
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
   },
 };
