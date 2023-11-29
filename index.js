55a3622efd54cc31c6d08d46e00aeaae6628ffef
YokotaYusuke <tarut8166@gmail.com>
2023-11-29 17:40:18 +0900

    yusukeの本番環境urlに変更（仮）

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const userInfo = require("./items");
-const cors = require("cors");
+const userInfo = require('./items');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173" })); //Need confirm to Frontend
-app.use(cors({ origin: "https://lenzzzz-frontend.onrender.com" })); //Need confirm to Frontend
+// app.use(cors({ origin: "https://lenzzzz-frontend.onrender.com" })); //Need confirm to Frontend
+app.use(cors({ origin: 'https://lenzzzz-frontend-cgi6.onrender.com' })); //Need confirm to Frontend
 
 //Controllre Func : Start
 const userInfoFunc = async (req, res) => {
@@ -16,17 +17,7 @@ const userInfoFunc = async (req, res) => {
 
 const registrationFunc = async (req, res) => {
   // console.log("req.body", req.body);
-  const {
-    id,
-    user_id,
-    purchase_date,
-    warranty_number,
-    warranty_unit,
-    product_name,
-    retailer,
-    warranty_photo,
-    product_photo,
-  } = req.body;
+  const { id, user_id, purchase_date, warranty_number, warranty_unit, product_name, retailer, warranty_photo, product_photo } = req.body;
 
   const appDate = await userInfo.save({
     id,
@@ -40,16 +31,16 @@ const registrationFunc = async (req, res) => {
     product_photo,
   });
 
-  res.status(201).send("追加しました");
+  res.status(201).send('追加しました');
 };
 
 const loginFunc = async (req, res) => {
   const { user_name, password } = req.body;
   // console.log("req::::::", req)
-  console.log('bodyより受信',user_name, password);
+  console.log('bodyより受信', user_name, password);
   const loginId = await userInfo.getByUserPass(user_name, password);
   if (!loginId) {
-    res.status(400).send("NG");
+    res.status(400).send('NG');
   } else {
     res.status(200).send(loginId);
   }
@@ -57,9 +48,9 @@ const loginFunc = async (req, res) => {
 //End : Controller Func
 
 //API : Start
-app.get("/items/:id", userInfoFunc);
-app.post("/registrations", registrationFunc);
-app.post("/login", loginFunc);
+app.get('/items/:id', userInfoFunc);
+app.post('/registrations', registrationFunc);
+app.post('/login', loginFunc);
 //End : API
 
 app.listen(PORT, () => {
