977e68ab383efa40f93b9fe01e4eba279fb925a4
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-11-30 14:45:47 +0900

    endpoint added

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // body-parserをインポート
+const express = require("express");
+const bodyParser = require("body-parser"); // body-parserをインポート
 const app = express();
-const userInfo = require('./items');
-const cors = require('cors');
+const userInfo = require("./items");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 // app.use(express.json());
 
 // 受信容量制限を変更
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
     return res.status(400).send({ message: err.message }); // あるいはカスタマイズされたエラーメッセージ
   }
   next();
 });
 
-
-const url = process.env.DEVELOPMENT_FRONTEND_URL || process.env.PRODUCTION_FRONTEND_URL;
+const url =
+  process.env.DEVELOPMENT_FRONTEND_URL || process.env.PRODUCTION_FRONTEND_URL;
 // app.use(cors({ origin: url })); //Need confirm to Frontend
-app.use(cors({ origin: 'http://localhost:5173' })); //Need confirm to Frontend
+app.use(cors({ origin: "http://localhost:5173" })); //Need confirm to Frontend
 // app.use(cors({ origin: "https://lenzzzz-frontend.onrender.com" })); //Need confirm to Frontend
 // app.use(cors({ origin: 'https://lenzzzz-frontend-cgi6.onrender.com' })); //Need confirm to Frontend
 
 //Controllre Func : Start
-const userInfoFunc = async (req, res) => {
-  const userId = Number(req.params.id);
-  const info = await userInfo.getById(userId);
+const getAllItems = async (req, res) => {
+  const _userId = Number(req.params.userId);
+  const info = await userInfo.getById(_userId);
   res.status(200).send(info);
+};
+
+const getSingleItems = async (req, res) => {
+  const _userId = Number(req.params.userId);
+  const info = await userInfo.getById(_userId);
+  res.status(200).send(info[req.params.index]);
 };
 
 const registrationFunc = async (req, res) => {
   // console.log("req.body", req.body);
-  const { id, user_id, purchase_date, warranty_number, warranty_unit, product_name, retailer, warranty_photo, product_photo } = req.body;
+  const {
+    id,
+    user_id,
+    purchase_date,
+    warranty_number,
+    warranty_unit,
+    product_name,
+    retailer,
+    warranty_photo,
+    product_photo,
+  } = req.body;
 
   const appDate = await userInfo.save({
     id,
@@ -46,7 +62,7 @@ const registrationFunc = async (req, res) => {
     product_photo,
   });
 
-  res.status(201).send('追加しました');
+  res.status(201).send("追加しました");
 };
 
 const loginFunc = async (req, res) => {
@@ -55,7 +71,7 @@ const loginFunc = async (req, res) => {
   // console.log('bodyより受信', user_name, password);
   const loginId = await userInfo.getByUserPass(user_name, password);
   if (!loginId) {
-    res.status(400).send('NG');
+    res.status(400).send("NG");
   } else {
     res.status(200).send(loginId);
   }
@@ -63,9 +79,11 @@ const loginFunc = async (req, res) => {
 //End : Controller Func
 
 //API : Start
-app.get('/items/:id', userInfoFunc);
-app.post('/registrations', registrationFunc);
-app.post('/login', loginFunc);
+app.get("/:userId/items", getAllItems);
+app.get("/:userId/items/:index", getSingleItems);
+// app.get('/items/:id', userInfoFunc);
+app.post("/registrations", registrationFunc);
+app.post("/login", loginFunc);
 //End : API
 
 app.listen(PORT, () => {
